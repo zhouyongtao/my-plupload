1c3fd447db88ac191f5cc1daf3249230351f31cc
irving <zhoyongtao@outlook.com>
2014-05-20 09:50:10 +0800

    Modify Code add log !

--- src/main/java/com/homeinns/web/servlet/DemoAction.java
+++ src/main/java/com/homeinns/web/servlet/DemoAction.java
@@ -1,15 +1,21 @@
 package com.homeinns.web.servlet;
+
 import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
 public class DemoAction extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
 		doPost(request, response);
 	}
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
 		request.setCharacterEncoding("utf-8");
 		response.setCharacterEncoding("utf-8");
 		String msg = "您上传了：<br/>";
@@ -17,7 +23,7 @@ public class DemoAction extends HttpServlet {
 		String[] fileNames = request.getParameterValues("fileName");
 		for (int i = 0; i < fileUrls.length; i++) {
 			msg += "文件路径：" + fileUrls[i] + " 文件名称：" + fileNames[i] + "<br/>";
-			// System.out.println(fileUrls[i] + " " + fileNames[i]);
+			System.out.println(fileUrls[i] + " " + fileNames[i]);
 		}
 		String name = request.getParameter("name");
 		msg += "用户名：" + name + "<br/>";
