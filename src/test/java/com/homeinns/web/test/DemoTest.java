099004508513ef000683ee3a9ed4698558b6eed8
irving <zhoyongtao@outlook.com>
2014-05-20 10:04:45 +0800

    修改测试代码

--- src/test/java/com/homeinns/web/test/DemoTest.java
+++ src/test/java/com/homeinns/web/test/DemoTest.java
@@ -1,7 +1,9 @@
 package com.homeinns.web.test;
-
 import junit.framework.TestCase;
 
-public class DemoTest extends TestCase {
-
+public class DemoTest  extends TestCase{
+	public void testName() {
+		String name = "I'am irving";
+		assertEquals("I'am irving", name);
+	}
 }
